b9fe2e414d6ef30c171c8ccda0dfc9a32c35aae1
Aliaksandr Hurynovich <aliaksandr@atomatiq.io>
2024-10-30 12:39:13 +0100

    fix/added firebase

--- src/App.tsx
+++ src/App.tsx
@@ -33,17 +33,14 @@ export default function App() {
   async function requestPermission() {
     //requesting permission using Notification API
     const permission = await Notification.requestPermission();
+    console.log("Permission : ", permission);
 
     if (permission === "granted") {
       const token = await getToken(messaging, {
         vapidKey: VITE_APP_VAPID_KEY,
       });
 
-      //We can send token to server
       console.log("Token generated : ", token);
-    } else if (permission === "denied") {
-      //notifications are blocked
-      alert("You denied for the notification");
     }
   }
 
